ec59ce9732c00b10be5716a0e261867540658e3e
galfa <fabiangal.45@gmail.com>
2024-03-27 16:22:41 +0000

    Added SQL injection

---
+++ README.md
@@ -0,0 +1,27 @@
+# Node app with OWASP vulnerabilities, part of Secure Application Programming module
+
+• SQL Injection.
+• XSS Reflected Dom based and stored.
+• Sensitive Data Exposure.
+
+## Prerequisites
+
+Node.js (and npm package manager)
+
+## Run
+
+1. Install npm packages
+
+```
+npm install
+```
+
+2. Run node
+
+```
+node index.js 
+OR
+mpm run dev
+```
+
+3. Open your browser to <http://localhost:3000>
--- index.js
+++ index.js
@@ -2,11 +2,87 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+//https://www.npmjs.com/package/sqlite3
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+    db.run("CREATE TABLE customers (name TEXT, nickname TEXT, age INTEGER, favorite_food TEXT)");
+});
+
+// Root redirects to the /customers 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.redirect('/customers');
+});
 
+// Log the used port and URL of the app
 app.listen(port, () => {
   console.log(`App listening on port ${port} | http://localhost:${port}`)
 })
+
+
+// View used to add customer via the browser.
+app.get('/add-customer', (req, res) => {
+    res.render('add_customer');
+});
+
+// Create a new customer
+app.post('/customers', (req, res) => {
+    const { name, nickname, age, favorite_food } = req.body;
+    const query = `INSERT INTO customers (name, nickname, age, favorite_food) VALUES (?, ?, ?, ?)`;
+
+    db.run(query, [name, nickname, age, favorite_food], function(err) {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.status(201).send(`Customer created with ID: ${this.lastID}`);
+        }
+    });
+});
+
+// Get all customers
+app.get('/customers', (req, res) => {
+    db.all("SELECT rowid, * FROM customers", [], (err, rows) => {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.render('index', { customers: rows });
+        }
+    });
+});
+  
+// Update a customer
+app.put('/customers/:id', (req, res) => {
+    const { name, nickname, age, favorite_food } = req.body;
+    const { id } = req.params;
+    const query = `UPDATE customers SET name = ?, nickname = ?, age = ?, favorite_food = ? WHERE rowid = ?`;
+    
+    db.run(query, [name, nickname, age, favorite_food, id], function(err) {
+    if (err) {
+        res.status(500).send(err.message);
+    } else if (this.changes === 0) {
+        res.status(404).send("Customer not found");
+    } else {
+        res.status(200).send(`Customer updated successfully`);
+    }
+    });
+});
+  
+
+// Delete a customer - adjusted for SQL injection 
+app.get('/delete-customer/:id', (req, res) => {
+    const id = req.params.id; 
+
+    db.run(`DELETE FROM customers WHERE rowid = ${id}`, function(err) {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.redirect('/customers');
+        }
+    });
+});
